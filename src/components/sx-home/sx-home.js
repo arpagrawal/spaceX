8f8a2a2eab427c2b7b1b744d63d8c9945505934b
Arpit Agrawal <arpagrawal@deloitte.com>
2021-01-16 17:41:08 +0530

    added data filter logic

--- src/components/sx-home/sx-home.js
+++ src/components/sx-home/sx-home.js
@@ -12,7 +12,8 @@ export default {
     data() {
       return {
         spacexData: [],
-        launchYears: []
+        launchYears: [],
+        currentQuery: {},
       };
     },
     directives: {},
@@ -27,14 +28,15 @@ export default {
      * function to handle flight data response
      */
       async getSpacexData(config) {
-        let configParams = new URLSearchParams({
+        let configParams = {
           limit: 100,
-        })
-
+        };
         if (config) {
-          configParams.append(...config);
+          const configObject = JSON.parse(config);
+          configParams = {...configParams, ...configObject};
         }
-        let response = await fetch(`${serviceUrls.launchData}?${configParams}`);
+        this.currentQuery = new URLSearchParams(configParams);
+        let response = await fetch(`${serviceUrls.launchData}?${this.currentQuery}`);
         let json = await response.json();
         return json;
       },
@@ -44,9 +46,8 @@ export default {
       handleSpacexDataResponse(res) {
         if (res) {
           this.spacexData = res;
-          this.launchYears = this.getLaunchYears(this.spacexData);
+          this.launchYears =  this.getLaunchYears(this.spacexData)
           this.updateURL();
-          console.log(res);
         }
       },  
 
@@ -65,11 +66,10 @@ export default {
         let queryUrl = '';
         const state = { page_id: 1, user_id: 5 };
         const title = '';
-        queryUrl += `?limit=100`;
+        queryUrl += `?${this.currentQuery}`;
         history.pushState(state, title, queryUrl);
       },
       getFilteredProducts(config) {
-        console.log(`bdkbdkkdbkjb${config}`);
         this.getSpacexData(config)
         .then(this.handleSpacexDataResponse)
         .catch(this.handleSpacexDataError);
